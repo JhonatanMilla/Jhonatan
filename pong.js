e8254ab85e551615cbe44ae9e78a959442bb2a35
JhonatanMilla <jhonatanmilla@prof.educacao.sp.gov.br>
2024-05-07 16:47:09 -0300

    Create pong.js

---
+++ pong.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+  stroke("blue")
+  fill("red")
+  mouseIsPressed
+  rect(mouseX, mouseY, 100, 150)
+  
+  
+}
